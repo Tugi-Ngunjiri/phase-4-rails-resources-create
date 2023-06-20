6b19a0623c328c300572a7e276fabf912cce2df4
tugi <blueqwinn@gmail.com>
2023-06-20 11:27:30 +0300

    add fetch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+    fetch("http://localhost:3000/birds", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: "Monk Parakeet",
+          species: "Myiopsitta monachus",
+        }),
+      });
+      
+
 end
